0544df7e52d8d343811d239eafab8c14a2d6c534
Nikhil Sundaraneedi <nikhilnaidu0004@gmail.com>
2025-06-20 23:19:48 +0530

    Implemented Fetch Method and FIxed Hero Img and Search Icon Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const API_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API
+const API_OPT = {
+  method : 'GET',
+  headers:{
+    accept: 'application/json',
+    Authorization : `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movieList,setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () =>{
+    setIsLoading(true);
+    try{
+      const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint,API_OPT);
+      if(!response.ok){
+        throw new Error (`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      if(data.Response === 'False'){
+        setMovieList([]);
+        throw new Error('Failed to fetch moives');
+      }
+      setMovieList(data.results || [])
+    }
+    catch(error){
+      toast.error(`Failed to fetch movies: ${error.message}`)
+    }finally{
+      setIsLoading(false);
+    }
+  }
+  useEffect(()=>{
+    fetchMovies();
+  },[])
   return (
     <main>
       <div className="pattern"/>
 
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" />
+          <img src="./src/assets/hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        
       </div>
+      <ToastContainer/>
     </main>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,7 +9,7 @@ const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <div className='search'>
         <div>
-            <img src='./assets/search.svg' alt='search'/>
+            <img src='./src/assets/search.svg' alt='search' />
             <input 
             type='text'
             placeholder='Search for a Movie..'
