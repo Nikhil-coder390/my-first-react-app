9fdc69a125b4c48ce6798c12f7a72cde7a9e5019
Nikhil Sundaraneedi <nikhilnaidu0004@gmail.com>
2025-07-05 23:43:20 +0530

    Connected Appwrite and Implemented Trending Movies Feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Search from './components/Search'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import MovieCard from './components/MovieCard'
-import Filter from './components/Filter'
 import { useDebounce } from 'react-use'
+import { updateSearchCount, displayTrendingMovie } from './appwrite'
 
 const API_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API
@@ -22,10 +22,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movieList,setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [language, setLanguage] = useState('');
-  const [year, setYear] = useState('');
-  const [rating, setRating] = useState('');
-  const [genre, setGenre] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [debounceSearchTerm,setDebounceSearchTerm] = useState('');
 
   useDebounce(()=> setDebounceSearchTerm(searchTerm),500,[searchTerm])
@@ -34,25 +31,18 @@ function App() {
     setIsLoading(true);
     try{
       let endpoint = (query)? `${API_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_URL}/discover/movie?sort_by=popularity.desc`;
-      
-      const params = new URLSearchParams();
-      if(!query){
-        if(language) params.append('with_original_language',language);
-        if(year) params.append('primary_release_year',year);
-        if(rating) params.append('vote_rating.gte', rating);
-        if(genre) params.append('with_genres', genre);
-      }
-
-      endpoint += `&${params.toString()}`;
 
       const response = await fetch(endpoint,API_OPT);
       if(!response.ok){
         throw new Error (`Error: ${response.status} ${response.statusText}`);
       }
       const data = await response.json();
-      console.log(data);
-      if(data.results) setMovieList(data.results);
-      else throw new Error('No Movies Found');
+      if(data.results){ setMovieList(data.results);}
+      else {throw new Error('No Movies Found');}
+
+      if(query && data.results.length>0){
+        await updateSearchCount(query,data.results[0]);
+      }
     }
     catch(error){
       toast.error(`Failed to fetch movies: ${error.message}`)
@@ -60,9 +50,23 @@ function App() {
       setIsLoading(false);
     }
   }
+
+  const loadTrendingMovies = async ()=>{
+    try{
+      const movies = displayTrendingMovie();
+      setTrendingMovies(movies);
+
+    }catch(error){
+      console.log(error);
+    }
+  }
   useEffect(()=>{
     fetchMovies(debounceSearchTerm);
-  },[debounceSearchTerm, language, year, rating, genre])
+  },[debounceSearchTerm])
+
+  useEffect(()=>{
+    loadTrendingMovies();
+  },[])
   return (
     <main>
       <div className="pattern"/>
@@ -72,16 +76,20 @@ function App() {
           <img src="./src/assets/hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-          <Filter
-            language={language}
-            setLanguage={setLanguage}
-            year = {year}
-            setYear = {setYear}
-            rating = {rating}
-            setRating = {setRating}
-            genre = {genre}
-            setGenre = {setGenre}/>
         </header>
+        {trendingMovies.length>0 && (
+          <section className='trending'>
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie,index)=>{
+                <li key={movie.$id}>
+                  <p>{index+1}</p>
+                  <img src={movie.poster_url} alt={movie.title}/>
+                </li>
+              })}
+            </ul>
+          </section>
+        )}
         <section className='all-movies'>
           <h2 className='m-3'>All Movies</h2>
           {isLoading ?(
---
+++ src/appwrite.js
@@ -0,0 +1,47 @@
+import { Client, Databases, Query,ID } from "appwrite";
+
+const project_id = import.meta.env.VITE_APPWRITE_PROJECT_ID
+const database_id = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const collection_id= import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const client = new Client().setEndpoint(endpoint).setProject(project_id)
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm,movie) => {
+    try{
+        const result = await database.listDocuments(database_id,collection_id, [
+            Query.equal('searchTerm',searchTerm),
+        ]);
+        if(result.documents.length>0){
+            const doc = result.documents[0];
+            await database.updateDocument(database_id,collection_id,doc.$id,{
+                count : doc.count+1,
+            })
+        }else{
+            await database.createDocument(database_id,collection_id,ID.unique(),{
+                searchTerm,
+                count:1,
+                movie_id :movie.id,
+                poster_url :  `https://image.tmdb.org./t/p/w/500${movie.poster_url}`,
+            })
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+    
+}
+
+export const displayTrendingMovie = async () => {
+    try{
+        const result = await database.listDocuments(database_id,collection_id,[
+            Query.limit(5),
+            Query.orderDesc("count")
+        ])
+        return result.documents;
+    }
+    catch(error){
+        console.log(error);
+    }
+} 
